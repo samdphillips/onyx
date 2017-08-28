8b853cf7f9a1071d5bb1eb4d62876324012c7017
Sam Phillips <samdphillips@gmail.com>
2017-08-28 01:18:25 +0000

    No instance variables to look up on intrinsics

--- src/onyx/objects/intrinsic.py
+++ src/onyx/objects/intrinsic.py
@@ -5,16 +5,22 @@ class UndefinedObject(Base):
     def __bool__(self):
         return False
 
+    def lookup_instance_variable(self, name):
+        pass
 
 class _True(Base):
     def __bool__(self):
         return True
 
+    def lookup_instance_variable(self, name):
+        pass
 
 class _False(Base):
     def __bool__(self):
         return False
 
+    def lookup_instance_variable(self, name):
+        pass
 
 true = _True()
 false = _False()
